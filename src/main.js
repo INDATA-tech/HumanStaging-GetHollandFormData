bc6412d0eb0591f9be87f22a4d4b565e3e02c74f
Onur Demir <101967110+onrdmr@users.noreply.github.com>
2024-09-25 15:54:11 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -28,7 +28,7 @@ export default async ({ req, res, log, error }) => {
 
     // ilgili cevabı al
     var answers = database.listDocuments("65dc57b1e8322b0426ae","65e97978db53e3998c12", [
-        Query.equal('EmailOfFilledBy', link.Email)
+        Query.equal('EmailOfFilledBy', "onur.demir@indata.com.tr")
     ]);
     log(answers);
     log(answers[0].RawData);
